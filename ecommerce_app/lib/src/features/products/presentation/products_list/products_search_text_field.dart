041684d68ac1a06e242e67032628671fa746c300
Aditya Jatnika <aditya.jatnika@zero-one-group.com>
2023-05-27 23:15:04 +0700

    feat: refactoring search and list product in main with getx. litle changes in purchase flow test, fake product repo test and robot in test/src

--- ecommerce_app/lib/src/features/products/presentation/products_list/products_search_text_field.dart
+++ ecommerce_app/lib/src/features/products/presentation/products_list/products_search_text_field.dart
@@ -1,26 +1,34 @@
 import 'package:ecommerce_app/src/features/products/presentation/products_list/products_search_state_provider.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get/get.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// Search field used to filter products by name
-class ProductsSearchTextField extends ConsumerStatefulWidget {
+class ProductsSearchTextField extends StatefulWidget {
   const ProductsSearchTextField({super.key});
 
   @override
-  ConsumerState<ProductsSearchTextField> createState() =>
+  State<ProductsSearchTextField> createState() =>
       _ProductsSearchTextFieldState();
 }
 
-class _ProductsSearchTextFieldState
-    extends ConsumerState<ProductsSearchTextField> {
+class _ProductsSearchTextFieldState extends State<ProductsSearchTextField> {
   final _controller = TextEditingController();
+  final ProductController _productController = Get.put(ProductController());
 
   @override
   void dispose() {
     // * TextEditingControllers should be always disposed
     _controller.dispose();
     super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // * Set the initial value of the controller
+    _productController.searchProducts('');
   }
 
   @override
@@ -32,27 +40,31 @@ class _ProductsSearchTextFieldState
       valueListenable: _controller,
       builder: (context, value, _) {
         return TextField(
-          controller: _controller,
-          autofocus: false,
-          style: Theme.of(context).textTheme.titleLarge,
-          decoration: InputDecoration(
-            hintText: 'Search products'.hardcoded,
-            icon: const Icon(Icons.search),
-            suffixIcon: value.text.isNotEmpty
-                ? IconButton(
-                    onPressed: () {
-                      _controller.clear();
-                      ref
-                          .read(productsSearchQueryStateProvider.notifier)
-                          .state = '';
-                    },
-                    icon: const Icon(Icons.clear),
-                  )
-                : null,
-          ),
-          onChanged: (text) =>
-              ref.read(productsSearchQueryStateProvider.notifier).state = text,
-        );
+            controller: _controller,
+            autofocus: false,
+            style: Theme.of(context).textTheme.titleLarge,
+            decoration: InputDecoration(
+              hintText: 'Search products'.hardcoded,
+              icon: const Icon(Icons.search),
+              suffixIcon: value.text.isNotEmpty
+                  ? IconButton(
+                      onPressed: () {
+                        _controller.clear();
+                        _productController.searchProducts('');
+                        // ref
+                        //     .read(productsSearchQueryStateProvider.notifier)
+                        //     .state = '';
+                      },
+                      icon: const Icon(Icons.clear),
+                    )
+                  : null,
+            ),
+            onChanged: (text) {
+              _productController.searchProducts(text);
+              // ref.read(productsSearchQueryStateProvider.notifier).state = text,
+            }
+            // ref.read(productsSearchQueryStateProvider.notifier).state = text,
+            );
       },
     );
   }
